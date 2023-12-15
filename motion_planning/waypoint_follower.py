2467d6a02f01872166dd1c5cad155c2b31cabd38
Nicholas Conlon <nicholas.conlon@colorado.edu>
2023-12-14 17:32:30 -0700

    waypoint follower working again

--- motion_planning/waypoint_follower.py
+++ motion_planning/waypoint_follower.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # Import necessary libraries
+import time
+
 import rospy  # Ros library
 import math  # Math library
-import sys
-import numpy as np
-from famsec.et_goa import et_goa
 from geometry_msgs.msg import Twist  # Twist messages
+from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry  # oOdometry messages
 from tf.transformations import euler_from_quaternion  # Quaternion conversions
 from gazebo_msgs.msg import ModelStates
+import numpy as np
 
 
 def extract_msg(data):
@@ -22,20 +23,29 @@ def extract_msg(data):
         pose = (lin.x, lin.y, lin.z)
         angle = euler_from_quaternion([ang.x, ang.y, ang.z, ang.w])
         return pose, angle
-    elif type(data) == Odometry:
-        x_pose = data.pose.pose.position.x  # Current x-position of the Jackal
-        y_pose = data.pose.pose.position.y  # Current y-position of the Jackal
-        z_pose = data.pose.pose.position.z  # Current z-position of the Jackal
+    elif type(data) == PoseStamped:
+        x_pose = data.pose.position.x  # Current x-position of the Jackal
+        y_pose = data.pose.position.y  # Current y-position of the Jackal
+        z_pose = data.pose.position.z  # Current z-position of the Jackal
         pose = (x_pose, y_pose, z_pose)
-        orientation_q = [data.pose.pose.orientation.x, data.pose.pose.orientation.y,
-                         data.pose.pose.orientation.z, data.pose.pose.orientation.w]
+        orientation_q = [data.pose.orientation.x, data.pose.orientation.y,
+                         data.pose.orientation.z, data.pose.orientation.w]
         # Convert quaternion to euler angles
         angle = euler_from_quaternion(orientation_q)
-        return pose, angle
+    else:
+        pose, angle = [], []
+    return pose, angle
 
 
 class WaypointFollower:
-    def __init__(self):
+    ASPEN = 'aspen'
+    GAZEBO = 'gazebo'
+    OUTDOOR = 'outdoor'
+    TARS = 'tars'
+    KIPP = 'kipp'
+    CASE = 'case'
+
+    def __init__(self, area, robot):
         self.x_dest = None
         self.y_dest = None
         self.pose_cmd_vel = None
@@ -43,18 +53,28 @@ class WaypointFollower:
         self.dist_err = 1
         self.K1 = 2
         self.K2 = 4
-        self.MQ = 0
+        self.max_speed = 0.25
+        self.timeout_secs = 1000
         # Initialize ros node
         rospy.init_node('go_to_waypoint', anonymous=True)
         # Set sleep rate
         self.sleep_rate = rospy.Rate(10)
 
-        # Initialize odometry subscriber
-        # grab_odom = rospy.Subscriber('/odometry/filtered/aspen', Odometry, calculate_heading)
-        self.grab_odom = rospy.Subscriber('/gazebo/model_states', ModelStates, self.calculate_heading)
-
-        # Initialize command velocity publisher
-        self.pub_vel = rospy.Publisher('/jackal_velocity_controller/cmd_vel', Twist, queue_size=10)
+        if area == self.ASPEN:
+            self.pose_sub = rospy.Subscriber('/{}/vrpn_client_node/cohrint_tars/pose'.format(robot),
+                                             PoseStamped, self.calculate_heading)
+            self.pub_vel = rospy.Publisher('/{}/jackal_velocity_controller/cmd_vel'.format(robot),
+                                           Twist, queue_size=10)
+        elif area == self.GAZEBO:
+            self.pose_sub = rospy.Subscriber('/gazebo/model_states', ModelStates,
+                                             self.calculate_heading)
+            # Initialize command velocity publisher
+            self.pub_vel = rospy.Publisher('/jackal_velocity_controller/cmd_vel', Twist,
+                                           queue_size=10)
+        elif area == self.OUTDOOR:
+            self.pose_sub = None # TODO
+            self.pub_vel = rospy.Publisher('/{}/jackal_velocity_controller/cmd_vel'.format(robot),
+                                           Twist, queue_size=10)
 
     def calculate_heading(self, data):
         """
@@ -63,7 +83,6 @@ class WaypointFollower:
         pose, angle = extract_msg(data)
         (x_pose, y_pose, z_pose) = pose[0], pose[1], pose[2]
         (y_rot, x_rot, z_rot) = angle[0], angle[1], angle[2]
-
         # Calculate x and y errors
         x_err = self.x_dest - x_pose
         y_err = self.y_dest - y_pose
@@ -86,23 +105,16 @@ class WaypointFollower:
 
         # Calculate command velocities
         if abs(angle_err) > 0.05:  # Only rotate
-            self.rot_cmd_vel = np.minimum(angle_err * self.K1, 0.5)
+            self.rot_cmd_vel = np.minimum(angle_err * self.K1, self.max_speed)
             self.pose_cmd_vel = 0
         else:  # Rotate and move
-            self.rot_cmd_vel = np.minimum(angle_err * self.K1, 0.5)
-            self.pose_cmd_vel = np.minimum(self.dist_err * self.K2, 0.5)
-
-        if self.pose_cmd_vel > 1.0:
-            self.pose_cmd_vel = 1.0
+            self.rot_cmd_vel = np.minimum(angle_err * self.K1, self.max_speed)
+            self.pose_cmd_vel = np.minimum(self.dist_err * self.K2, self.max_speed)
 
     def reset(self):
         """
         Reset the parameters
         """
-        self.x_dest = None
-        self.y_dest = None
-        self.pose_cmd_vel = None
-        self.rot_cmd_vel = None
         self.dist_err = 1
 
     def go_to_waypoint(self, _x, _y):
@@ -111,30 +123,50 @@ class WaypointFollower:
         """
         self.x_dest = _x
         self.y_dest = _y
-        ###
-        self.et_object = et_goa()
-        pred_paths = ['/data/webots/rollout{}_state.npy'.format(x) for x in np.arange(0, 10)]
-        self.et_object.set_pred_paths(pred_paths)
-        self.et_object.preprocess()
-        goa = self.et_object.get_goa_times(self.goa_threshold, 0)
-        ###
 
         # Go to waypoint
         while self.dist_err > 0.1:
             vel = Twist()
             vel.linear.x = self.pose_cmd_vel
             vel.angular.z = self.rot_cmd_vel
-
             self.pub_vel.publish(vel)
             self.sleep_rate.sleep()
+
+        print("Done")
+
+    def go_to_waypoints(self, _xs, _ys):
+        """
+        Navigate to a waypoint at (_x, _y)
+        """
+        t_max = self.timeout_secs
+        t0 = time.time()
+        for _x, _y in zip(_xs, _ys):
+            self.dist_err = 1.0
+            self.x_dest = _x
+            self.y_dest = _y
+
+            # Go to waypoint
+            while self.dist_err > 0.1 and abs(t0 - time.time()) < t_max:
+                vel = Twist()
+                vel.linear.x = self.pose_cmd_vel
+                vel.angular.z = self.rot_cmd_vel
+                self.pub_vel.publish(vel)
+                self.sleep_rate.sleep()
+            print("At waypoint")
+        print("Waypoints complete")
 
 
 if __name__ == '__main__':
     try:
-        wp = WaypointFollower()
-        x_dest = float(sys.argv[1])
-        y_dest = float(sys.argv[2])
-        wp.go_to_waypoint(x_dest, y_dest)
-        # wp.reset()
+        # a circle of waypoints
+        points = np.linspace(0, 2 * np.pi, 25)
+        xs = 2 + 2 * np.cos(points)
+        ys = 2 + 2 * np.sin(points)
+        # xs = [0, 10, 15, 0]
+        # ys = [0, 20, 0, 0]
+        mission_area = WaypointFollower.GAZEBO
+        robot = WaypointFollower.TARS
+        wp = WaypointFollower(area=mission_area, robot=robot)
+        wp.go_to_waypoints(xs, ys)
     except rospy.ROSInterruptException:
         pass
