90a89ecb3c26793049e09017b48d44f4a3410eb6
nick conlon <nickconlon1@gmail.com>
2023-12-21 10:55:52 -0700

    updates to mission manager

--- base_interface/mission_management.py
+++ base_interface/mission_management.py
@@ -8,16 +8,21 @@ from motion_planning import projections
 
 class MissionManager:
     def __init__(self, mission_area_image_path):
-        self.home = (501, 717)
-        self.poi_b = (523, 151)
-        self.poi_a = (402, 515)
-        self.poi_c = (594, 360)
-        self.poi_d = (685, 676)
+        lat_center, lon_center = 40.01045433, 105.24432153
+        area_miny, area_maxy, area_minx, area_maxx = -50, 50, -50, 50
+        self.projector = projections.Projector(lat_center, lon_center)
+        self.projector.setup()
+        pois = self.projector.get_pois()
+        self.poi_a = pois[0]
+        self.poi_b = pois[1]
+        self.poi_c = pois[2]
+        self.poi_d = pois[3]
+        self.home = pois[4]
 
         self.current_plan = None
         self.visualize = False
         self.mission_area_image = np.asarray(Image.open(mission_area_image_path))
-        self.mission_area_bounds = np.array([[-50, 50], [-50, 50]])
+        self.mission_area_bounds = np.array([[area_miny, area_maxy], [area_minx, area_maxx]])
 
     def update_plan(self, new_plan):
         self.current_plan = new_plan
@@ -44,7 +49,7 @@ class MissionManager:
         else:
             self.delete_plan()
         if poi:
-            self.plan_waypoints(*self.home, *poi)
+            self.plan_waypoints(self.home.x, self.home.y, poi.x, poi.y)
 
     def plan_waypoints(self, robot_x, robot_y, goal_x, goal_y):
         # RRT goal = [y, x]
@@ -62,32 +67,6 @@ class MissionManager:
         plan = np.vstack((np.array([robot_x, robot_y]), plan))
         self.update_plan(plan)
 
-    def t_overlay_image(self):
-        img = self.mission_area_image.copy()
-        fig, ax = plt.subplots(frameon=False)
-        self.visualize = False
-
-        test_g = self.poi_c
-        for s, g, color in zip([self.home, test_g], [test_g, self.home], ['black', 'red']):
-            self.delete_plan()
-            self.plan_waypoints(*s, *g)
-            y, x, _ = img.shape
-            plt.imshow(img)
-            plt.xlim([0, x])
-            plt.ylim([y, 0])
-            p = self.current_plan
-            p = np.vstack((s, p))
-            plt.plot(p[:, 0], p[:, 1], '--', c=color, markersize=10)
-            plt.scatter(p[:, 0], p[:, 1], c=color, s=10)
-        plt.axis('off')
-        plt.tight_layout()
-        canvas = plt.gca().figure.canvas
-        canvas.draw()
-        data = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
-        img = data.reshape(canvas.get_width_height()[::-1] + (3,))
-        plt.close(fig)
-        return img
-
     def get_overlay_image(self, path_color='black'):
         img = self.mission_area_image.copy()
         fig, ax = plt.subplots(frameon=False)
@@ -96,9 +75,13 @@ class MissionManager:
         plt.xlim([0, x])
         plt.ylim([y, 0])
         if self.has_plan():
-            p = self.current_plan
-            plt.plot(p[:, 0], p[:, 1], '--', c=path_color, markersize=10)
-            plt.scatter(p[:, 0], p[:, 1], c=path_color, s=10)
+            plan = self.current_plan
+            pixel_plan = []
+            for point in plan:
+                pixel_plan.append(self.projector.cartesian_to_pixel(point[0], point[1]))
+            pixel_plan = np.array(pixel_plan)
+            plt.plot(pixel_plan[:, 0], pixel_plan[:, 1], '--', c=path_color, markersize=10)
+            plt.scatter(pixel_plan[:, 0], pixel_plan[:, 1], c=path_color, s=10)
         plt.axis('off')
         plt.tight_layout()
         canvas = plt.gca().figure.canvas
@@ -110,23 +93,8 @@ class MissionManager:
 
 
 if __name__ == '__main__':
-    lat_center = 40.01045433
-    lon_center = 105.24432153
-
-    pp = projections.Projector(lat_center, lon_center)
-    pp.setup()
-    pois = pp.get_pois()
-    home = pois[-1]
-    tgt = pois[1]
-
     m = MissionManager('../base_interface/mission_area.png')
-    m.plan_waypoints(home.x, home.y, tgt.x, tgt.y)
-    c = m.current_plan
-    cc = []
-    for p in c:
-        px, py = pp.cartesian_to_pixel(p[0], p[1])
-        cc.append(np.array([px, py]))
-    m.current_plan = np.array(cc)
+    m.plan_waypoints(m.home.x, m.home.y, m.poi_c.x, m.poi_c.y)
     img = m.get_overlay_image()
     plt.imshow(img)
     plt.axis('off')
