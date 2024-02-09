ad4e3ff252154605b38b1ce79fa898b3c73e895a
Nicholas Conlon <nicholas.conlon@colorado.edu>
2024-02-08 18:15:36 -0700

    adding training scenarios

--- start_waypoint.sh
+++ start_waypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source ../../devel/setup.bash
-python3 main_waypoint_follower.py -s ./scenarios/settings_m4.yaml
+python3 main_waypoint_follower.py -s ./scenarios/settings_aspen.yaml
